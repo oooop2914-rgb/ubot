427826014b50306f5184dcb2530d9299ab054cf2
oooop2914-rgb <oooop2914@gmail.com>
2026-03-14 14:01:10 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -3,17 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-MAX_BOT = int(os.getenv("MAX_BOT", "60"))
+MAX_BOT = int(os.getenv("MAX_BOT", "100"))
 
-DEVS = list(map(int, os.getenv("DEVS", "6787098484").split()))
+DEVS = list(map(int, os.getenv("DEVS", "6938178028").split()))
 
-API_ID = int(os.getenv("API_ID", "23205651"))
+API_ID = int(os.getenv("API_ID", "32839580"))
 
-API_HASH = os.getenv("API_HASH", "5a5d93f4564092db12126729cacf58ac")
+API_HASH = os.getenv("API_HASH", "2b6cfe9b5f1600365925e17d71f5fb34")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8011986562:AAFkkSt-EkH6l37rDB3WpiV2sPv3_KkvyWs")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8545876781:AAEsz1Xt3VekhBP8d2KFINL_DDYY0ANm8NUs")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "8150600900"))
+OWNER_ID = int(os.getenv("OWNER_ID", "6938178028"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002053287763 -1002044997044 -1002022625433 -1002050846285 -1002400165299 -1002416419679 -1001473548283").split()))
 
